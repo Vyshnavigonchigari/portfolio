462ac514d3e72e81493e6261df6e838c26321799
vyshnavi <vyshnavigonchigari@gmail.com>
2025-11-20 01:06:50 +0530

    remane aubot.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import Hero from "./components/Hero.jsx";
 import Navbar from "./components/Navbar.jsx";
-import About from "./components/about.jsx";
+import About from "./components/About.jsx";
 import Skills from "./components/Skills.jsx";
 import Projects from "./components/Projects.jsx";
 import Contact from "./components/Contact.jsx"; 

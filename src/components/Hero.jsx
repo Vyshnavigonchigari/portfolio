81dfa16c47e0937016d2a2a89e02ae25831cb2c1
vyshnavi <vyshnavigonchigari@gmail.com>
2025-11-21 16:10:00 +0530

    Fixed resume button

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,9 @@
-import { FaGithub, FaLinkedinIn, FaEnvelope, FaFileDownload } from "react-icons/fa";
+import {
+  FaGithub,
+  FaLinkedinIn,
+  FaEnvelope,
+  FaFileDownload,
+} from "react-icons/fa";
 import Typewriter from "typewriter-effect";
 
 export default function Hero() {
@@ -9,18 +14,16 @@ export default function Hero() {
     >
       {/* Left content */}
       <div className="w-full md:w-1/2 text-center md:text-left mt-0 md:mt-10 mb-20 md:mb-0">
-        <h1 className="text-2xl  lg:text-4xl font-bold mb-1 md:mb-3 text-white ">
+        <h1 className="text-[28px]  lg:text-4xl font-bold mb-1 md:mb-3 text-white ">
           Hey I'm{" "}
           <span className="bg-linear-to-r from-cyan-400 via-purple-500 to-purple-300 text-transparent bg-clip-text">
             Vyshnavi
           </span>
         </h1>
 
-        <h2 className="text-xl lg:text-3xl font-semibold mb-1 md:mb-2 flex justify-center md:justify-start items-center gap-2">
+        <h2 className="text-[22px] lg:text-3xl font-semibold mb-1 md:mb-2 flex justify-center md:justify-start items-center gap-2">
           <span className="text-white">I'm a</span>
-          <span
-            className=" text-xl lg:text-3xl bg-linear-to-r from-pink-500 via-orange-400 to-purple-400 text-transparent bg-clip-text drop-shadow-lg inline-flex"
-          >
+          <span className=" text-xl lg:text-3xl bg-linear-to-r from-pink-500 via-orange-400 to-purple-400 text-transparent bg-clip-text drop-shadow-lg inline-flex">
             <Typewriter
               options={{
                 strings: ["Fullstack Developer", "Software Developer"],
@@ -39,27 +42,36 @@ export default function Hero() {
         {/* Social icons */}
         <div className="flex justify-center md:justify-start gap-4  mt-3 md:mt-6">
           {[
-            { icon: <FaGithub />, link: "https://github.com/Vyshnavigonchigari" },
-            { icon: <FaLinkedinIn />, link: "https://www.linkedin.com/in/vyshnavi-gonchigari" },
-            { icon: <FaEnvelope />, link: "mailto:vyshnavigonchigari@gmail.com" },
+            {
+              icon: <FaGithub />,
+              link: "https://github.com/Vyshnavigonchigari",
+            },
+            {
+              icon: <FaLinkedinIn />,
+              link: "https://www.linkedin.com/in/vyshnavi-gonchigari",
+            },
+            {
+              icon: <FaEnvelope />,
+              link: "mailto:vyshnavigonchigari@gmail.com",
+            },
           ].map(({ icon, link }, i) => (
             <a
               key={i}
               href={link}
               target="_blank"
               rel="noopener noreferrer"
-              className="w-8 h-8 flex items-center justify-center rounded-full border-0 text-white text-xl hover:scale-110 hover:bg-linear-to-r hover:from-cyan-500 hover:via-purple-500 hover:to-pink-500 transition-all duration-300"
+              className="w-10 h-10 flex items-center justify-center rounded-full border-0 text-white text-2xl hover:scale-110 hover:bg-linear-to-r hover:from-cyan-500 hover:via-purple-500 hover:to-pink-500 transition-all duration-300"
             >
               {icon}
             </a>
           ))}
         </div>
-<a href="/Vyshnavi_Gonchigari_In_Resume.pdf" download > 
-  <button className="flex items-center justify-center gap-2 mt-4 md:mt-6  mx-auto  md:ml-0 px-2 py-1 md:px-4 md:py-2 text-white bg-linear-to-r from-cyan-500 via-purple-500 to-pink-500 font-bold rounded-md hover:scale-110 hover:border-2 hover:border-white shadow-md transition">
-    <FaFileDownload className="text-lg" />
-    Resume
-  </button>
-</a>
+        <a href="/Vyshnavi_Gonchigari_In_Resume.pdf" download>
+          <button className="flex items-center justify-center gap-2  px-4 py-2 mt-4 md:mt-6  mx-auto  md:mx-0   text-white font-semibold  bg-linear-to-r from-cyan-500 via-purple-500 to-pink-500  rounded-md hover:scale-110 hover:border hover:border-white shadow-md transition">
+            <FaFileDownload className="text-xl" />
+            Resume
+          </button>
+        </a>
       </div>
 
       {/* Right image */}
